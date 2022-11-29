6114bca0cfc230542479e8a771a12b619bb87474
PrasathGowtham <108874931+PrasathGowtham@users.noreply.github.com>
2022-11-29 12:53:00 +0530

    Add files via upload

---
+++ src/projectData.js
@@ -0,0 +1,37 @@
+
+const projects = [
+    {
+      title: 'MOVIEE TICKET BOOKING APP',
+      image: './images/ticket',
+      description: ' A Ticket booking app is a booking application which is a model of Moviee ticket booking app ',      
+      tools: ['React','Node Js', 'bcrypt', 'MongoDB', 'Express','JWT' ,'Express'],
+      github: 'https://github.com/PrasathGowtham',
+      link: 'https://eloquent-nasturtium-e44b56.netlify.app/ticket',
+    },
+    {
+      title: 'DASH BOARD CLONE',
+      image: './images/dashbord',
+      description: 'This is a basic front end Dash board clone Application.',
+      tools: ['React','HTML','Css','Bootstarp'],
+      github: 'https://github.com/PrasathGowtham',
+      link: 'https://bejewelled-shortbread-4e8972.netlify.app/',
+    },
+    {
+      title: 'Nationalize API',
+      image: './images/nation',
+      description: 'It is based on	By Searching the Name of Person, We get the response back by displaying the top two countries of Name and it also display the probability value..',
+      tools: ['HTML','Css','Javascript'],
+      github: 'https://github.com/PrasathGowtham',
+      link: 'https://gilded-sprinkles-278914.netlify.app',
+    },
+    {
+      title: 'E-COMMERSE SITE',
+      image: './images/ecomm',
+      description: 'The E-commerse site is a basic front end react app ',
+      tools: ['React','HTML','Css','Bootstarp'],
+      github: 'https://github.com/PrasathGowtham',
+      link: 'https://rad-churros-eac9aa.netlify.app/?#',
+    },
+  ];
+  
+  export default projects;
